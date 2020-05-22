dba4503eb0e7f44d60937d087c6949a4a6f660f2
heropoo <aiyouyou1000@163.com>
2020-05-22 15:53:39 +0800

    wh

--- wb.php
+++ wb.php
@@ -13,9 +13,9 @@ if ($content['password'] != '123qwe') {
 $cmd = "sudo git pull && npm run build";
 
 $descriptorspec = array(
-    0 => array("pipe", "r"),  // 标准输入，子进程从此管道中读取数据
-    1 => array("pipe", "w"),  // 标准输出，子进程向此管道中写入数据
-    2 => array("pipe", "w") // 标准错误
+    0 => array("pipe", "r"),    // 标准输入，子进程从此管道中读取数据
+    1 => array("pipe", "w"),    // 标准输出，子进程向此管道中写入数据
+    2 => array("pipe", "w")     // 标准错误
 );
 
 $cwd = dirname(__DIR__);
